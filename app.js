dca3d32cd55990c371f222d2327f54fb9de47c6b
KadirTopal <kadirkenantopal@gmail.com>
2022-09-14 14:15:04 -0400

    js updated

--- app.js
+++ app.js
@@ -1,22 +1,3 @@
-/* const kavustay = '28 Sept 2022 14:35:00';
-
-
-function countDown(){
-  const kavustayDate = new Date(kavustay);
-  const currentDate = new Date();
-  const totalSeconds = (kavustayDate - currentDate) / 1000;
-  const date = Math.floor(totalSeconds / 3600 / 24) %30;
-  const hours = Math.floor(totalSeconds / 3600) % 24;
-  const mins = Math.floor(totalSeconds / 60) % 60;
-  const seconds = Math.floor(totalSeconds) % 60; 
-  console.log(hours, mins, seconds)
-}
-
-
-countDown();
-
-setInterval(countDown, 1000); */
-
 const countDown = () => {
   const countDate = new Date('Sept 28, 2022 14:35:00').getTime();
   const currentTime = new Date().getTime();
